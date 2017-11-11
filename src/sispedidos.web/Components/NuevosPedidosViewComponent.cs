2b506c5e9fef10cc8dce95561ccfbc2cc7d63513
vstudiowebcurso <hhernandeza@hotmail.com>
2017-11-11 12:21:00 -0500

    Componente web que indica si hay nuevos pedidos

---
+++ src/sispedidos.web/Components/NuevosPedidosViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sispedidos.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sispedidos.web.Components
+{
+    public class NuevosPedidosViewComponent : ViewComponent
+    {
+        private sispedidosDbContext _db;
+        public NuevosPedidosViewComponent(sispedidosDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pedidos = await _db.Docus.ToListAsync();
+
+            return View("NuevosPedidosTag", pedidos);
+        }
+    }
+}
